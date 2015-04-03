cffdab03d1e923118feb5703ed94593856cac8b3
Sam Bolgert <sbolgert@gmail.com>
2015-04-02 20:58:12 -0700

    Replace ngmin with grunt-ng-annotate
    ngmin has been deprecated.
    Update references to ngmin to ngAnnotate

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,7 +224,7 @@ module.exports = function (grunt) {
 
     // Allow the use of non-minsafe AngularJS files. Automatically makes it
     // minsafe compatible so Uglify does not destroy the ng references
-    ngmin: {
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -360,7 +360,7 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     'cdnify',
     'cssmin',
