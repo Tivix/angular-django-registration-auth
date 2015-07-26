9d1cfd13e64620c12e33b44ec143997e02e4f703
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-07-26 16:47:13 -0700

    Converted app to use ui.router

--- app.js
+++ app.js
@@ -4,107 +4,117 @@ angular.module('angularDjangoRegistrationAuthApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute',
+    'ui.router',
 ])
-    .config(function ($routeProvider, $httpProvider) {
-        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/register', {
-                templateUrl: 'views/register.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/passwordReset', {
-                templateUrl: 'views/passwordreset.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/passwordResetConfirm/:firstToken/:passwordResetToken', {
-                templateUrl: 'views/passwordresetconfirm.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/login', {
-                templateUrl: 'views/login.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/verifyEmail/:emailVerificationToken', {
-                templateUrl: 'views/verifyemail.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/logout', {
-                templateUrl: 'views/logout.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/userProfile', {
-                templateUrl: 'views/userprofile.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/passwordChange', {
-                templateUrl: 'views/passwordchange.html',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/restricted', {
-                templateUrl: 'views/restricted.html',
-                controller: 'RestrictedCtrl',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus();
-                    }],
-                }
-            })
-            .when('/authRequired', {
-                templateUrl: 'views/authrequired.html',
-                controller: 'AuthrequiredCtrl',
-                resolve: {
-                    authenticated: ['djangoAuth', function (djangoAuth) {
-                        return djangoAuth.authenticationStatus(true);
-                    }],
-                }
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
+    .config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
+        function ($stateProvider, $urlRouterProvider, $httpProvider) {
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+            $urlRouterProvider.otherwise('home');
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'views/main.html',
+                    controller: 'MainCtrl',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('register', {
+                    url: '/register',
+                    templateUrl: 'views/register.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('passwordReset', {
+                    url: '/passwordReset',
+                    templateUrl: 'views/passwordreset.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('passwordResetConfirm', {
+                    url: '/passwordResetConfirm/:firstToken/:passwordResetToken',
+                    templateUrl: 'views/passwordresetconfirm.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('login', {
+                    url: '/login',
+                    templateUrl: 'views/login.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('verifyEmail', {
+                    url: '/verifyEmail/:emailVerificationToken',
+                    templateUrl: 'views/verifyemail.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('logout', {
+                    url: '/logout',
+                    templateUrl: 'views/logout.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('userProfile', {
+                    url: '/userProfile',
+                    templateUrl: 'views/userprofile.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('passwordChange', {
+                    url: '/passwordChange',
+                    templateUrl: 'views/passwordchange.html',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('restricted', {
+                    url: 'restricted',
+                    templateUrl: 'views/restricted.html',
+                    controller: 'RestrictedCtrl',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus();
+                        }],
+                    }
+                })
+                .state('authRequired', {
+                    url: '/authRequired',
+                    templateUrl: 'views/authrequired.html',
+                    controller: 'AuthrequiredCtrl',
+                    resolve: {
+                        authenticated: ['djangoAuth', function (djangoAuth) {
+                            return djangoAuth.authenticationStatus(true);
+                        }],
+                    }
+                });
+        }])
     .run(function (djangoAuth) {
         djangoAuth.initialize('//localhost:9050/api/v1/auth', false);
     });
--- controllers/verifyemail.js
+++ controllers/verifyemail.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('angularDjangoRegistrationAuthApp')
-    .controller('VerifyemailCtrl', function ($scope, $routeParams, djangoAuth) {
-        djangoAuth.verify($routeParams["emailVerificationToken"]).then(function (data) {
+    .controller('VerifyemailCtrl', function ($scope, $stateParams, djangoAuth) {
+        djangoAuth.verify($stateParams["emailVerificationToken"]).then(function (data) {
             $scope.success = true;
         }, function (data) {
             $scope.failure = false;
